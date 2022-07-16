82bf9db83b324893f2c7f2648b8b4bd22137be0b
Sean <seanpoulter@hotmail.com>
2022-07-16 00:37:27 -0400

    Finish project. Random colors not added.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const body = document.body;
 const layout = document.getElementsByClassName('layout')[0];
+const btn = document.button;
+let newGrid
 
 body.appendChild(layout);
 
@@ -13,4 +15,32 @@ for (let grid = 0; grid < 256; grid++) {
     })
 }
 
+document.querySelector('button').addEventListener('click', () => {
+    newGrid = parseInt(prompt("Input new grid size (under 100)."))
+    if (newGrid > 100) {
+        alert("Too High!");
+    }
+    else if (newGrid <= 100) {
+        newGame(newGrid);
+    }
+    else  {
+        alert("Error");
+    }
+});
 
+function newGame(newGrid) {
+    layout.textContent = "";
+    for (let grid = 0; grid < (newGrid * newGrid); grid++) {
+        console.log("test")
+        const board = document.createElement('div');
+        board.style.height = `calc(70vw / ${newGrid})`;
+        board.style.width = `calc(100%/${newGrid})`;
+        board.style.maxHeight = `calc(600px/${newGrid})`;
+        board.className = "base";
+        board.style.border = "black solid 1px"
+        layout.appendChild(board);
+        board.addEventListener('mouseover', () => {
+            board.classList.add('hover');
+        })
+    }
+}
